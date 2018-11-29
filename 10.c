27225ef8aee98c6ea36ff644fddb2c6ac4e6a8e2
이예현 <shszyh@iclooud.com>
2018-11-29 16:27:09 +0900

    10

--- 10.c
+++ 10.c
@@ -8,48 +8,28 @@
 		c=='\r'||
 		c=='(' ||
 		c==')')
+		{
 		return 1;
-		
+		}
 	return 0;
 	}
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	FILE* fp;
-	char name[100];
-	char word[100];
-	int c,cnt;
-
-	
-	//printf,scanf
-	printf("input a file name: ");
-	scanf("%s",name);
-	
-	//fopen
-	fp=fopen(name,"r");
-	
-	
-	
-	//error fondling
-	if (fp==NULL)
-	{
-		printf("error! check the file name:%s\n", name);
-		return -1;
-	}
-	
+int fget_word(FILE* fp, char* word)
+{
+	char c;
+	int cnt;
 	while((c=fgetc(fp)) != EOF)
-	{
-		if(is_whitespace(c)==0)//유의미한 문자이면 나감 
-		{
-			break;
-		}
-}
-		if(c==EOF)//최초 읽은 문자가 마지막 일때 
-		{
-			return 0;
-		}
-		
-		cnt=0;
+   {
+      if(is_whitespace(c) == 0){
+         break;
+      }
+   }
+      if (c == EOF){
+         return 0;
+      }
+      	//본격적으로 단어를 한글자씩 읽음 
+		//word processing
+    	cnt=0;
 		word[cnt++]=c;
 		word[cnt]='\0';
 		
@@ -62,12 +42,38 @@ int main(int argc, char *argv[]) {
 			}
 			
 			cnt++;
+		}
+	return cnt;
+	
+}
+int main(int argc, char *argv[]) {
+	FILE* fp;
+	char name[100];
+	char word[100];
+
+
+	
+	//printf,scanf
+	printf("input a file name: ");
+	scanf("%s",name);
+	
+	//fopen
+	fp=fopen(name,"r");
 		
-		
-		//본격적으로 단어를 한글자씩 읽음 
-		//word processing
-		printf("%s\n",word);//test code
+	//error fondling
+	if (fp==NULL)
+	{
+		printf("error! check the file name:%s\n", name);
+		return -1;
 	}
+	while (fget_word(fp,word) != 0)
+	{
+		printf("%s\n",word);
+	}
+	
+	printf("%c\n",fgetc(fp));
+
+	
 	//fclose
 	fclose(fp);
 	return 0;
